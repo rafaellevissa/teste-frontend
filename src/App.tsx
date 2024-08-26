8d9985251684114e3a632de5bbde8b4e7aa512cd
rafaellevissa <rafaellevissa@gmail.com>
2024-08-26 12:03:06 -0300

    Import to App component weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Weather from "./components/Weather/Weather";
+
 export default function App() {
   return (
     <main>
       <Navbar />
+      <Weather />
     </main>
   );
 }
